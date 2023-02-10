a90fa2dfdf03ba2b1c9b9f570648ccd2d12598ec
Matthew Gilligan <mcgilligan97@gmail.com>
2023-02-10 12:20:50 +0900

    Create comin soon page

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { ReactComponent as IconLinkedIn } from './assets/icons/linkedin.svg';
+import Logo from './assets/logos/logo.png'
+import "./App.css";
+
+class App extends React.Component {
+  render = () => {
+    return (
+      <div className="card">
+        <div className="header">
+          <div className="logo">
+            <img src={Logo} alt="New Edge" />
+          </div>
+          <div className="social">
+            <a href="https://www.linkedin.com/company/newedgestudio/" title="LinkedIm" target="_blank" rel="noopener noreferrer">
+              <IconLinkedIn className="icon" />
+            </a>
+          </div>
+        </div>
+        <div className="content">
+          <div className="title-holder">
+            <h1>Just a little longer...</h1>
+            <p>Website coming soon. Please check back to know more. Shoot us an email if you're curious.</p>
+          </div>
+          <a href="mailto:evan@newedgestud.io">
+            <div className="cta">Send us an email</div>
+          </a>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
